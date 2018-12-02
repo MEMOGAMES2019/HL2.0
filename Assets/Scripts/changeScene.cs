79cd3e244c0c7adfcdf5f76a06a62f3e18893e47
MiguelJiRo <mn372920@gmail.com>
2018-12-02 23:38:54 +0100

    Primeros pasos

---
+++ Assets/Scripts/changeScene.cs
@@ -0,0 +1,20 @@
+﻿using RAGE.Analytics;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class changeScene : MonoBehaviour
+{
+    public int num;
+
+    // Use this for initialization
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            SceneManager.LoadScene(num);
+            Debug.Log("Scene changed to " + num);
+        }
+    }
+
+}
