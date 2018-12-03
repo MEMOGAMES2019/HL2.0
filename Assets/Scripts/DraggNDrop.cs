7ae55eedc35d60a3ec71f48e12298a3ab080e9ec
DavidCogolludo <davipe05@ucm.es>
2018-12-03 22:02:45 +0100

    Segunda version

---
+++ Assets/Scripts/DraggNDrop.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(BoxCollider2D))]
+
+public class DraggNDrop : MonoBehaviour {
+
+    private Vector3 startPoint;
+    Luggage luggage;
+    Vector3 offset;
+    void Start () {
+        luggage = GameObject.Find("Maleta").GetComponent<Luggage>();
+	}
+
+    void OnMouseDown()
+    {
+        startPoint = transform.position;
+        offset = gameObject.transform.position -
+            Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 10.0f));
+    }
+
+    void OnMouseDrag()
+    {
+        Vector3 newPosition = new Vector3(Input.mousePosition.x, Input.mousePosition.y, 10.0f);
+        transform.position = Camera.main.ScreenToWorldPoint(newPosition) + offset;
+    }
+    void OnMouseUp()
+    {
+        if (!luggage.isCorrect())
+            transform.position = startPoint;
+        else Destroy(gameObject);
+    }
+}
---
+++ Assets/Scripts/Luggage.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Luggage : MonoBehaviour {
+
+    public string[] ropa;
+    bool correct = false;
+    
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        int i = 0;
+        while (i < ropa.Length && collision.gameObject.name != ropa[i]) { i++; }
+        if(i < ropa.Length)
+            correct = true;
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        correct = false;
+    }
+    
+    public bool isCorrect()
+    {
+        if (correct)
+        {
+            correct = false;
+            return true;
+        }
+        else
+            return false;
+    }
+}
