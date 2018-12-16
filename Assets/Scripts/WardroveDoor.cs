b77605f5ccb0a45e9735a8bfaf9cea2817e502ac
Manuel Hidalgo Lorente <manuhida@ucm.es>
2018-12-16 21:06:39 +0100

    # Se unifican los script de las puerta de los armarios

---
+++ Assets/Scripts/WardroveDoor.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts
+{
+    class WardroveDoor : MonoBehaviour
+    {
+        #region Atributos
+
+        /// <summary>
+        /// Objeto que representa la puerta del armario en el estado contrario.
+        /// </summary>
+        public GameObject puertaOtroEstado;
+
+        /// <summary>
+        /// Indica si la puerta está activa al inicio o no
+        /// </summary>
+        public bool active;
+
+        #endregion
+
+        #region Propiedades
+
+        #endregion
+
+        #region Eventos
+
+        private void Start()
+        {
+            gameObject.SetActive(active);
+        }
+
+        void OnMouseDown()
+        {
+            puertaOtroEstado.gameObject.SetActive(true);
+            gameObject.SetActive(false);
+        }
+
+        #endregion
+    }
+}
